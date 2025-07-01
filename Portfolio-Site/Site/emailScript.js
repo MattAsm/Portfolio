d12dc1109452ec565432fecfacc74a2eee0524a2
MattAsm <67015529+MattAsm@users.noreply.github.com>
2025-07-01 15:01:17 -0400

    Removes projects from this repo
    Created separate repo's for each project. Didn't know you could make multiple projects like this. Shrunk this projects file and link to other repos on GitHub!

--- Portfolio-Site/Site/emailScript.js
+++
@@ -1,9 +0,0 @@
-const senderName = document.getElementById("name-input");
-const message = document.getElementById("message");
-
-function templateEmail(){
-    const sender = senderName.value;
-    const mesg = message.value;
-    location.href = `mailto:matthew.asmunt@yahoo.ca?subject=${encodeURIComponent(sender)}&body=${encodeURIComponent(mesg)}`;
-    console.log(sender + " " + mesg);
-}
