7701c7ef46706d65770d789bae31ca4b2b021604
MattAsm <67015529+MattAsm@users.noreply.github.com>
2025-08-11 12:54:42 -0400

    Added my image

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ import Footer from './Components/Footer';
 function App() {
 
   return (
-    <>
+    <div>
       <Header />
         <main>
-          <p>This the main body</p>
+          <a href="https://www.linkedin.com/in/matthew-asmunt/" title='LinkedIn' target='_blank'><img src='src\assets\Me.jpg' alt="" id='me'/></a>
+          <h4 id='description'>I am a programmer, web designer, videogame developer, and animator. This is a collection of my
+            applications and websites.</h4>
+          <br /><hr />
+          
         </main>
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,17 @@ function Footer(){
 
     return(
         <div className='headerFooter'>
-            <h1>Footer</h1>
+            <h6>Where all good pages come to rest</h6>
+            <p>&copy; Copyright Matthew Asmunt <br />
+            
+                <a href="https://www.linkedin.com/in/matthew-asmunt/" target="_blank">
+                    LinkedIn
+                </a>
+                 &nbsp;
+                 <a href="https://mattasm.github.io/" target="_blank">
+                    Portfolio
+                </a>
+            </p>
         </div>
     );
 }
